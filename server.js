e7ac284ec40f75539a785f28af85d2564df420b9
Sergius92739 <sergem279@gmail.com>
2024-01-25 23:02:58 +0300

    fixed

--- server.js
+++ server.js
@@ -37,15 +37,10 @@ const fortune = (ctx, body = null, status = 200) => {
     setTimeout(() => {
       // Uncomment for error generation
 
-       if (Math.random() > 0.8) {
-         reject(new Error('Something bad happened'));
-         return;
-       } 
-      // if (true) {
-      //   reject(new Error('Something bad happened'));
-      //   return;
-      // }
-      
+      //  if (Math.random() > 0.8) {
+      //    reject(new Error('Something bad happened'));
+      //    return;
+      //  }
 
       ctx.response.status = status;
       ctx.response.body = body;
